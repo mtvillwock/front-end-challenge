359217e0cfa6e77de5e6ef99f4653658f491eed0
Matthew Villwock <mtvillwock@gmail.com>
2015-12-08 23:41:27 -0800

    add overlay show/hide

--- script.js
+++ script.js
@@ -39,6 +39,16 @@
             })
         }
 
+        view.handleMouseOver = function() {
+            $('.image-container img').on('mouseenter', function() {
+                $(this).siblings('.overlay').toggleClass('hidden');
+            })
+
+            $('.image-container').on('mouseleave', function() {
+                $(this).children('.overlay').addClass('hidden');
+            })
+        }
+
         return view;
     }
 
@@ -66,30 +76,31 @@
         controller.init = function() {
             productView.showLoadingGif();
             controller.getProducts()
-                .then(function(response) {
-                    controller.getTemplate()
-                        .then(function(template) {
+            .then(function(response) {
+                controller.getTemplate()
+                .then(function(template) {
 
-                            var products = response.sales;
-                            var template = template;
+                    var products = response.sales;
+                    var template = template;
 
-                            products.forEach(function(productData, index) {
-                                controller.addProduct(productData, index)
-                            })
+                    products.forEach(function(productData, index) {
+                        controller.addProduct(productData, index)
+                    })
 
-                            productCtrl.products.forEach(function(product) {
-                                product.updatehtml(template);
-                            })
+                    productCtrl.products.forEach(function(product) {
+                        product.updatehtml(template);
+                    })
 
-                            productView.updateDOM(productCtrl.products);
-                            productView.handleDelete();
-                            productView.hideLoadingGif();
-                        }, function(err) {
-                            console.log("error fetching products")
-                        })
+                    productView.updateDOM(productCtrl.products);
+                    productView.handleDelete();
+                    productView.handleMouseOver();
+                    productView.hideLoadingGif();
                 }, function(err) {
                     console.log("error fetching products")
                 })
+            }, function(err) {
+                console.log("error fetching products")
+            })
         }
         return controller;
     }
@@ -101,6 +112,7 @@
         var product = {};
         product.photo = productData.photos.medium_half;
         product.title = productData.name;
+        product.description = productData.description;
         product.tagline = productData.tagline;
         product.url = productData.url;
         product.htmlview = "";
@@ -109,41 +121,16 @@
 
         product.updatehtml = function(template) {
             product.htmlview = template.replace('{image}', product.photo)
-                .replace('{title}', product.title)
-                .replace('{tagline}', product.tagline)
-                .replace('{url}', product.url)
-                .replace('{custom_class}', product.custom_class);
+            .replace('{title}', product.title)
+            .replace('{description}', product.description)
+            .replace('{tagline}', product.tagline)
+            .replace('{url}', product.url)
+            .replace('{custom_class}', product.custom_class);
         }
 
         return product;
     }
 
     productCtrl.init();
-
-    // $.getJSON('data.json')
-    // .then(function(response) {
-    //     $.get('product-template.html')
-    //         .then(function(template) {
-
-    //             var products = response.sales;
-    //             var template = template;
-
-    //             products.forEach(function(productData, index) {
-    //                 var product = new productObj(response.sales[index], index);
-    //                 productCtrl.products.push(product);
-    //             })
-
-    //             productCtrl.products.forEach(function(product) {
-    //                 product.updatehtml(template);
-    //             })
-
-    //             productView.updateDOM(productCtrl.products);
-    //             productView.handleDelete();
-    //         }, function(err) {
-    //             console.log("error fetching products")
-    //         })
-    // }, function(err) {
-    //     console.log("error fetching products")
-    // })
 
 }());
