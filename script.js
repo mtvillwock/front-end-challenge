a84cc1a3f682d6b4e1c1155ae11d8c84de4cb24a
Matthew Villwock <mtvillwock@gmail.com>
2015-12-08 21:22:52 -0800

    delete btn; add View obj

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // IIFE to avoid polluting global space
 (function app() {
-    // Controller to handle products
-    function ProductController() {
-        var controller = {};
-        controller.products = [];
 
-        controller.updateDOM = function() {
+    function ProductView() {
+        var view = {};
+
+        view.updateDOM = function(products) {
             var i = 0
             thishtml = '';
-            for (i = 0; i < controller.products.length; i++) {
+            for (i = 0; i < products.length; i++) {
                 if (i % 3 == 0) {
                     thishtml += "<div class='row'>";
                     console.log("START")
                 }
-                thishtml += controller.products[i].htmlview;
-                if ((i % 3 == 2) || i == (controller.products.length - 1)) {
+                thishtml += products[i].htmlview;
+                if ((i % 3 == 2) || i == (products.length - 1)) {
                     thishtml += "</div>";
                     console.log("FINISH")
                 }
@@ -23,6 +22,22 @@
             $("#content").append(thishtml)
         }
 
+        view.handleDelete = function() {
+            $('.delete-product').on('click', function() {
+                $(this).parent().remove();
+            })
+        }
+
+        return view;
+    }
+
+    // immediately create view
+    var productView = new ProductView();
+
+    // Controller to handle products
+    function ProductController() {
+        var controller = {};
+        controller.products = [];
         return controller;
     }
     // immediately create controller
@@ -67,7 +82,8 @@
                         product.updatehtml(template);
                     })
 
-                    productCtrl.updateDOM();
+                    productView.updateDOM(productCtrl.products);
+                    productView.handleDelete();
                 }, function(err) {
                     console.log("error fetching products")
                 })
@@ -76,65 +92,3 @@
         })
 
 }());
-
-// function domobj() {
-//     var self = this;
-//     self.products = [];
-
-//     self.getproducts = function(url) {
-//         $.getJSON(url, function(response) {
-//             for (i = 0; i < response.sales.length; i++) {
-//                 self.products.push(new productobj(response.sales[i], i));
-//             }
-//             console.log(self.products[1])
-//         });
-//     }
-
-//     self.updateproducthtml = function() {
-//         for (i = 0; i < self.products.length; i++) {
-//             self.products[i].updatehtml();
-//         }
-//     }
-
-//     self.updatedom = function() {
-//         var i = 0
-//         thishtml = '';
-//         for (i = 0; i < self.products.length; i++) {
-//             if (i % 3 == 0) {
-//                 thishtml += "<div class='row'>";
-//                 console.log("START")
-//             }
-//             thishtml += self.products[i].htmlview;
-//             if ((i % 3 == 2) || i == (self.products.length - 1)) {
-//                 thishtml += "</div>";
-//                 console.log("FINISH")
-//             }
-//         }
-//         $("#content").append(thishtml)
-//     }
-
-// }
-
-// function productobj(product, i) {
-//     var self = this;
-//     self.photo = product.photos.medium_half
-//     self.title = product.name
-//     self.tagline = product.tagline
-//     self.url = product.url
-//     self.htmlview = ""
-//     self.index = i
-//     self.custom_class = "col" + ((i % 3) + 1)
-
-//     self.updatehtml = function() {
-//         $.get('product-template.html', function(template) {
-//             console.log("template is:", template)
-//             self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
-//         });
-//     }
-// }
-
-
-// var page = new domobj();
-// page.getproducts('data.json');
-// setTimeout("console.log('building html');page.updateproducthtml();", 20);
-// setTimeout("page.updatedom()", 1000)
